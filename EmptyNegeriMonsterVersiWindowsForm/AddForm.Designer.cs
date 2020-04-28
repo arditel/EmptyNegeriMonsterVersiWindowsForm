64d08ae49626a1c97561c58d14e651fcefef6dbf
arditel <arditel@gmail.com>
2020-04-28 10:37:59 +0700

    Initialize

---
+++ EmptyNegeriMonsterVersiWindowsForm/AddForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace EmptyNegeriMonsterVersiWindowsForm
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.namaTextBox = new System.Windows.Forms.TextBox();
+            this.jenisComboBox = new System.Windows.Forms.ComboBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jenis:";
+            // 
+            // namaTextBox
+            // 
+            this.namaTextBox.Location = new System.Drawing.Point(72, 24);
+            this.namaTextBox.Name = "namaTextBox";
+            this.namaTextBox.Size = new System.Drawing.Size(263, 20);
+            this.namaTextBox.TabIndex = 2;
+            // 
+            // jenisComboBox
+            // 
+            this.jenisComboBox.FormattingEnabled = true;
+            this.jenisComboBox.Location = new System.Drawing.Point(72, 67);
+            this.jenisComboBox.Name = "jenisComboBox";
+            this.jenisComboBox.Size = new System.Drawing.Size(263, 21);
+            this.jenisComboBox.TabIndex = 3;
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(179, 113);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(260, 113);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 156);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.jenisComboBox);
+            this.Controls.Add(this.namaTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddForm";
+            this.Text = "Tambah Monster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox namaTextBox;
+        private System.Windows.Forms.ComboBox jenisComboBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ EmptyNegeriMonsterVersiWindowsForm/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace EmptyNegeriMonsterVersiWindowsForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monstersListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.actionTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.eatButton = new System.Windows.Forms.Button();
+            this.fireButton = new System.Windows.Forms.Button();
+            this.flyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monstersListBox
+            // 
+            this.monstersListBox.FormattingEnabled = true;
+            this.monstersListBox.Location = new System.Drawing.Point(12, 31);
+            this.monstersListBox.Name = "monstersListBox";
+            this.monstersListBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.monstersListBox.Size = new System.Drawing.Size(171, 134);
+            this.monstersListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Daftar Monster:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Daftar Aksi:";
+            // 
+            // actionTextBox
+            // 
+            this.actionTextBox.Location = new System.Drawing.Point(12, 203);
+            this.actionTextBox.Multiline = true;
+            this.actionTextBox.Name = "actionTextBox";
+            this.actionTextBox.ReadOnly = true;
+            this.actionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.actionTextBox.Size = new System.Drawing.Size(354, 101);
+            this.actionTextBox.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(190, 31);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(176, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Tambah Monster";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // eatButton
+            // 
+            this.eatButton.Location = new System.Drawing.Point(190, 60);
+            this.eatButton.Name = "eatButton";
+            this.eatButton.Size = new System.Drawing.Size(176, 23);
+            this.eatButton.TabIndex = 5;
+            this.eatButton.Text = "Makan Cacing 100 Tahun";
+            this.eatButton.UseVisualStyleBackColor = true;
+            // 
+            // fireButton
+            // 
+            this.fireButton.Location = new System.Drawing.Point(189, 89);
+            this.fireButton.Name = "fireButton";
+            this.fireButton.Size = new System.Drawing.Size(176, 23);
+            this.fireButton.TabIndex = 6;
+            this.fireButton.Text = "Semburan Api Maha Dahsyat";
+            this.fireButton.UseVisualStyleBackColor = true;
+            // 
+            // flyButton
+            // 
+            this.flyButton.Location = new System.Drawing.Point(189, 118);
+            this.flyButton.Name = "flyButton";
+            this.flyButton.Size = new System.Drawing.Size(176, 23);
+            this.flyButton.TabIndex = 7;
+            this.flyButton.Text = "Terbang Mengitari Bumi";
+            this.flyButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 316);
+            this.Controls.Add(this.flyButton);
+            this.Controls.Add(this.fireButton);
+            this.Controls.Add(this.eatButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.actionTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monstersListBox);
+            this.Name = "MainForm";
+            this.Text = "Aplikasi Negeri Monster Versi Windows Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox monstersListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox actionTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button eatButton;
+        private System.Windows.Forms.Button fireButton;
+        private System.Windows.Forms.Button flyButton;
+    }
+}
+
